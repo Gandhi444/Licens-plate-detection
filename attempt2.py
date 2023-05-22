18c950a877a5b1fbad71523ff27c2e0bbfbe6d89
Antoni <antonio.borowski@onet.pl>
2023-05-22 19:14:16 +0000

    fisrst stage of plate detection

--- attempt2.py
+++ attempt2.py
@@ -3,9 +3,10 @@ import json
 from pathlib import Path
 import cv2 as cv,cv2
 import procesing.process
-from procesing.process import empty_callback
+from procesing.process import empty_callback, rect_distance
 import numpy as np
 import os
+
 
 color_imgs=[]
 grey_images=[]
@@ -14,7 +15,7 @@ for images in os.listdir(folder_dir):
     # check if the image ends with png
     if (images.endswith(".jpg")):
         input=cv.imread(folder_dir+'/'+images,cv.IMREAD_COLOR)
-        scale_percent = 10 # percent of original size
+        scale_percent = 20 # percent of original size
         width = int(input.shape[1] * scale_percent / 100)
         height = int(input.shape[0] * scale_percent / 100)
         dim = (width, height)
@@ -26,20 +27,10 @@ for images in os.listdir(folder_dir):
 
 # resize image
 
-# input = cv.resize(input, dim, interpolation = cv.INTER_AREA)
-# input_gray= cv.cvtColor(input, cv.COLOR_BGR2GRAY)
-# input=cv.imread('data/train/2023-05-08 (1).jpg',cv.IMREAD_COLOR)
-# scale_percent = 10 # percent of original size
-# width = int(input.shape[1] * scale_percent / 100)
-# height = int(input.shape[0] * scale_percent / 100)
-# dim = (width, height)
-# # resize image
-
-# input = cv.resize(input, dim, interpolation = cv.INTER_AREA)
-# input_gray= cv.cvtColor(input, cv.COLOR_BGR2GRAY)
 cv2.namedWindow('processed',)
-cv2.createTrackbar('erosion', 'processed', 1, 100, empty_callback)
-cv2.createTrackbar('low', 'processed', 0, 255, empty_callback)
+cv2.createTrackbar('erosion', 'processed', 1, 20000, empty_callback)
+cv2.createTrackbar('filtr', 'processed', 1, 200, empty_callback)
+cv2.createTrackbar('low', 'processed', 1, 255, empty_callback)
 cv2.createTrackbar('high', 'processed', 0, 255, empty_callback)
 cv2.createTrackbar('photo nr', 'processed', 0, len(grey_images)-1, empty_callback)
 
@@ -50,26 +41,87 @@ while True:
         break
     # get current positions of four trackbars
     erosion_size = cv2.getTrackbarPos('erosion', 'processed')
+    filtr_size = cv2.getTrackbarPos('filtr', 'processed')
     low = cv2.getTrackbarPos('low', 'processed')
     high = cv2.getTrackbarPos('high', 'processed')
     nr = cv2.getTrackbarPos('photo nr', 'processed')
     proccesed=grey_images[nr]
 
-    #proccesed=cv.GaussianBlur(proccesed,(9,9),0)
-    proccesed= cv2.bilateralFilter(proccesed, 11, low, low) 
-    proccesed = cv.adaptiveThreshold(proccesed,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
-              cv.THRESH_BINARY,11,2)
-    kernel = np.ones((high, high), np.uint8)
-    proccesed=cv.dilate(proccesed,kernel)
+    #proccesed= cv2.bilateralFilter(proccesed, 13, filtr_size, filtr_size) 
+    
+    #_,proccesed=cv2.threshold(proccesed,low,255,cv.THRESH_BINARY)
+    #proccesed=cv2.GaussianBlur(proccesed,(filtr_size*2+1,filtr_size*2+1),0)
+    #proccesed=cv.medianBlur(proccesed,2*filtr_size+1)
+    # proccesed = cv.adaptiveThreshold(proccesed,255,cv.ADAPTIVE_THRESH_MEAN_C,\
+    #          cv.THRESH_BINARY_INV,low*2+1,high)
+    shape=proccesed.shape
+    frame=0.06
+    proccesed= cv2.bilateralFilter(proccesed, 13, filtr_size, filtr_size) 
+    proccesed = cv2.Canny(proccesed,low,high)
+
+    proccesed=cv.dilate(proccesed,np.ones((2)))
+    lower_blue = np.array([100,80,80])
+    upper_blue = np.array([120,255,255])
+    hsv = cv2.cvtColor(color_imgs[nr], cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv,lower_blue,upper_blue)
+    mask=cv.erode(mask,np.ones((12)))
+    mask=cv.dilate(mask,np.ones((10)))
+    blue_cnts,new = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    blue_cnts = sorted(blue_cnts, key = cv2.contourArea, reverse = True)
+    img_blue_cnts=color_imgs[nr].copy()
+    euroband=None
+    for c in blue_cnts:
+        x,y,w,h=cv2.boundingRect(c)
+        if w/h>0.3 and w/h<1:
+            euroband=c
+            break
+    proccesed[:,0:round(shape[1]*frame)]=0
+    proccesed[0:round(shape[0]*frame),:]=0
+    proccesed[:,shape[1]-round(shape[1]*frame):]=0
+    proccesed[shape[0]-round(shape[0]*frame):,:]=0
+    if euroband is not None:
+        x,y,w,h=cv2.boundingRect(euroband)
+        proccesed[:,0:round(x-shape[0]*frame)]=0 
 
 
-    kernel = np.ones((erosion_size, erosion_size), np.uint8)
-    proccesed=cv.erode(proccesed,kernel)
+    cnts,new = cv2.findContours(proccesed.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:30]
+    candidates=[]
+    for c in cnts:
+        x,y,w,h=cv2.boundingRect(c)
+        ratio=h/w
+        #print(ratio)
+        if ratio>0.1 and ratio<0.5 and cv2.arcLength(c,closed=True)<50000:
+            candidates.append(c)
 
+    img_cnts=cv2.drawContours(color_imgs[nr].copy(),cnts,-1,(0,255,0),2)
+    img_cand=cv2.drawContours(color_imgs[nr].copy(),candidates,-1,(255,0,0),2)
+    img_cand=cv2.drawContours(img_cand,[euroband],-1,(0,0,255),2)
 
     cv2.imshow('processed', proccesed)
+    cv2.imshow('cnt',img_cnts)
+    cv2.imshow('cand',img_cand)
 
 
+
+    cv2.imshow('cand',img_cand)
+    if euroband is not None and len(candidates)>0:
+        best_fit=0
+        best_dist=np.inf
+        euroband_rect=cv2.boundingRect(euroband)
+        for i in range(len(candidates)):
+            c_rect=cv2.boundingRect(candidates[i])
+            dist=rect_distance(euroband_rect,c_rect)
+            #print(dist)
+            if dist<best_dist:
+                best_dist=dist
+                best_fit=i
+        x,y,w,h = cv2.boundingRect(candidates[best_fit]) 
+        plate=grey_images[nr].copy()[y:y+h,x:x+w]
+        cv2.imshow('plate',plate)
+    #cv2.imshow('color',img_blue_cnts)
 
 
 cv.destroyAllWindows()
+
+
--- procesing/process.py
+++ procesing/process.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 
 def perform_processing(image: np.ndarray) -> str:
     print(f'image.shape: {image.shape}')
@@ -8,3 +8,34 @@ def perform_processing(image: np.ndarray) -> str:
 
 def empty_callback(value):
     pass
+
+def rect_distance(rect1,rect2):
+    x1, y1, w1, h1=rect1
+    x2, y2, w2, h2=rect2
+    x1b=x1+w1
+    y1b=y1+h1
+    x2b=x2+w2
+    y2b=y2+h2
+
+    left = x2b < x1
+    right = x1b < x2
+    bottom = y2b < y1
+    top = y1b < y2
+    if top and left:
+        return math.dist((x1, y1b), (x2b, y2))
+    elif left and bottom:
+        return math.dist((x1, y1), (x2b, y2b))
+    elif bottom and right:
+        return math.dist((x1b, y1), (x2, y2b))
+    elif right and top:
+        return math.dist((x1b, y1b), (x2, y2))
+    elif left:
+        return x1 - x2b
+    elif right:
+        return x2 - x1b
+    elif bottom:
+        return y1 - y2b
+    elif top:
+        return y2 - y1b
+    else:             # rectangles intersect
+        return 0.
